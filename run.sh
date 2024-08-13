6a2c37cee91cb1163e3940f479512b591ba89bac
asher <guowenwu>
2024-08-13 02:11:30 -0700

    add sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash  
+ray_count=$1  
+catkin_make  
+source devel/setup.sh  
+if [ "$ray_count" == '16' ]; then  
+    roslaunch aloam_velodyne aloam_velodyne_VLP_16.launch  
+elif [ "$ray_count" == '32' ]; then  
+    roslaunch aloam_velodyne aloam_velodyne_HDL_32.launch  
+elif [ "$ray_count" == '64' ]; then  
+    roslaunch aloam_velodyne aloam_velodyne_HDL_64.launch  
+else  
+    roslaunch aloam_velodyne aloam_mulran.launch  
+fi  
+echo "done"
